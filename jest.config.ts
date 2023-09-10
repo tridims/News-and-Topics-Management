c5be8a8ea0a5d64d1c9aef139cd0fe7b5a4ba878
Dimas Tri Mustakim <test31122001@gmail.com>
2023-09-10 14:04:44 +0700

    Init prisma module

--- jest.config.ts
+++ jest.config.ts
@@ -1,15 +1,5 @@
-/* eslint-disable */
+import { getJestProjects } from '@nx/jest';
+
 export default {
-  displayName: 'news-and-topics-management',
-  preset: './jest.preset.js',
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
-  },
-  moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: './coverage/news-and-topics-management',
-  testMatch: [
-    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
-    '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
-  ],
+  projects: getJestProjects(),
 };
