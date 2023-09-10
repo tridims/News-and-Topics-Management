251cba00eface18f57909368cb7004ba4e3bbff9
Dimas Tri Mustakim <test31122001@gmail.com>
2023-09-10 17:15:14 +0700

    done building my own prisma plugin module

--- modules/prisma/src/index.ts
+++ modules/prisma/src/index.ts
@@ -1,1 +1,41 @@
-export * from './lib/prisma';
+import { PrismaClient } from '@prisma/client';
+import { FastifyPluginAsync } from 'fastify';
+import fp from 'fastify-plugin';
+
+const prisma = new PrismaClient();
+
+const PrismaPlugin: FastifyPluginAsync<PrismaPluginOptions> = async (
+  fastify,
+  options
+) => {
+  fastify
+    .decorate('prisma', prisma)
+    .addHook('onClose', async (fastifyInstance, done) => {
+      await fastifyInstance.prisma.$disconnect();
+      done();
+    });
+
+  // fastify
+  //   .decorateRequest('prisma', prisma)
+  //   .addHook('onClose', async (request, done) => {
+  //     await request.prisma.$disconnect();
+  //     done();
+  //   });
+};
+
+declare module 'fastify' {
+  interface FastifyInstance {
+    prisma: PrismaClient;
+  }
+  // interface FastifyRequest {
+  //   prisma: PrismaClient;
+  // }
+}
+
+export interface PrismaPluginOptions {
+  datasourceUrl?: string;
+}
+
+export default fp(PrismaPlugin, {
+  name: 'fastify-prisma'
+});
